5a7e920a0ab5c25de9b1f994c8f114f876fb20f8
ckorir <chrisruto.k@gmail.com>
2023-11-30 19:06:22 +0300

    Completed sentence

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -2,3 +2,33 @@
 
 class MyString:
   pass
+  def __init__(self, value=""):
+    self._value = value
+
+  @property
+  def get_value(self):
+    return self._value
+
+  def set_value(self, new_value):
+    if isinstance(new_value, str):
+      self._value = new_value
+    else:
+      print("The value must be a string.")
+
+  def is_sentence(self):
+    return self._value.endswith(".")
+
+  def is_question(self):
+    return self._value.endswith("?")
+
+  def is_exclamation(self):
+    return self._value.endswith("!")
+
+  def count_sentences(self):
+    odered_sentence = self._value.replace('!', '.').replace('?', '.')
+    # Split the text into sentences based on the '.' character
+    sentences = [s for s in odered_sentence.split('.') if s]
+    return len(sentences)
+  
+  value = property(get_value, set_value)
+  
